ca7acace1589082e8cf05220d6423d3e80d244ab
Saajid <saajidf@gmail.com>
2020-03-11 16:47:54 -0400

    cleanup

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ import os
 from bs4 import BeautifulSoup
 import re
 import csv
-import operator
 import timeit
 import math
 
 numOfDocs = 0
 docFreq = {}
 tf = {}
+l1 = []
 
 
-def calc_weight(word):
-    idf = math.log(numOfDocs / docFreq[word])
-    return tf * idf
+def calc_weight(word, freq):
+    idf = math.log(float(numOfDocs) / float(docFreq[word]))
+    return float(freq) * float(idf)
 
 
 if __name__ == '__main__':
     # start timer
-    # start = timeit.default_timer()
-    print('what the')
+    start = timeit.default_timer()
+    stop_file = open('stopwords.txt', 'r')
+    for line in stop_file.readlines():
+        for i in line.split('\n'):
+            l1.append(i)
+    stop_file.close()
+
     files = os.listdir("html_files")
 
     # get term frequency, number of files, and doc frequency of each word
@@ -53,9 +58,36 @@ if __name__ == '__main__':
                 else:
                     docFreq[y] = 1
 
+        for k, v in list(wordCount.items()):
+            if v == 1:
+                del wordCount[k]
+        for k, v in list(wordCount.items()):
+            if k in l1:
+                del wordCount[k]
+        for k, v in list(wordCount.items()):
+            if len(k) == 1:
+                del wordCount[k]
+
         # add term frequency of current file to dictionary of all tf dictionaries
         tf[newFile] = wordCount
 
-    print(docFreq['the'])
+    # loop through dic of tf dic and create doc weights
+    for t in tf:
+        weights = {}
 
-    # print(timeit.default_timer() - start)
+        # only look at current tf
+        name = t
+        curr = tf[t]
+
+        # calc weight of each word in each doc
+        for x in curr:
+            weights[x] = calc_weight(x, curr[x])
+
+        csv_file = name + '.csv'
+        create = "output/" + csv_file
+        os.makedirs(os.path.dirname(create), exist_ok=True)
+        w = csv.writer(open(create, "w"))
+        for key, val in weights.items():
+            w.writerow([key, val])
+
+    print(timeit.default_timer() - start)
